ad09f444796a82cf7df7e61c435016c1a5c3f341
Chans <imagechans@gmail.com>
2024-06-02 20:41:13 +0800

    Update serializers.py
    init

--- user_profile/serializers.py
+++ user_profile/serializers.py
@@ -1,18 +1,36 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from .models import Profile
 
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('name',)
+
 class UserProfileSerializer(serializers.ModelSerializer):
+    groups = serializers.SerializerMethodField()
+
     class Meta:
         model = Profile
-        fields = ('phone_number', 'address')
+        fields = ('phone_number', 'address', 'groups')
+
+    def get_groups(self, obj):
+        user = obj.user
+        groups = user.groups.all()
+        return GroupSerializer(groups, many=True).data
 
 class UserSerializer(serializers.ModelSerializer):
     profile = UserProfileSerializer()
+    groups = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'profile')
-        read_only_fields = ('username', 'email')
+        fields = ('username', 'email', 'profile', 'groups')
+        read_only_fields = ('username', 'email', 'groups')
+
+    def get_groups(self, obj):
+        groups = obj.groups.all()
+        return GroupSerializer(groups, many=True).data
 
     def update(self, instance, validated_data):
         profile_data = validated_data.pop('profile', {})
